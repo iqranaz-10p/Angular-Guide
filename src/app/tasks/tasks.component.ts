e02e0103a49639719c6299e8beebe704e6c633a3
iqranaz-10p <iqra.naz@10pearls.com>
2024-11-23 01:04:50 +0500

    output user specific task, conditional rendering component and deleting tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,40 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { DUMMY_TASKS } from '../dummy-tasks';
+import { NgFor, NgIf } from '@angular/common';
+import { Task } from './task/task.model';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, NgFor, NewTaskComponent, NgIf],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
+  @Input() userId!: string;
   @Input() name?: string;
+  tasks = DUMMY_TASKS;
+  isAddingTask: boolean = false;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+  trackByTaskId(index: number, task: Task) {
+    return task.id;
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
 }
