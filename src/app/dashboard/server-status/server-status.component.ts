6a6944ec9b02df1b9699faf3903e48d39c81dcb3
iqranaz-10p <iqra.naz@10pearls.com>
2024-12-06 20:18:54 +0500

    added dynamic class binding

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -9,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class ServerStatusComponent {
 
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
